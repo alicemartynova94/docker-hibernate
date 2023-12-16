9ae6a16a274a3b8e48411fe77e6356e3df70bb31
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-16 20:41:18 +0300

    add new entity class Country and all necessary fields

--- src/main/java/domain/Country.java
+++ src/main/java/domain/Country.java
@@ -1,2 +1,68 @@
-package domain;public class Country {
+package domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(schema = "world", name = "country")
+@Data
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "code_2", nullable = false)
+    private String code2;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "continent", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    private Continent continent;
+
+    @Column(name = "region", nullable = false)
+    private String region;
+
+    @Column(name = "surface_area", nullable = false)
+    private BigDecimal surfaceArea;
+
+    @Column(name = "indep_year")
+    private Short independenceYear;
+
+    @Column(name = "population", nullable = false)
+    private Integer population;
+
+    @Column(name = "life_expectancy")
+    private BigDecimal lifeExpectancy;
+
+    @Column(name = "gnp")
+    private BigDecimal GNP;
+
+    @Column(name = "gnpo_id")
+    private BigDecimal GNPOId;
+
+    @Column(name = "local_name", nullable = false)
+    private String localName;
+
+    @Column(name = "government_form", nullable = false)
+    private String governmentForm;
+
+    @Column(name = "head_of_state")
+    private String headOfState;
+
+    @OneToOne
+    @JoinColumn(name = "capital")
+    private City city;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    private Set<CountryLanguage> languages;
 }
