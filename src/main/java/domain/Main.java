6d0f79a700c4a78358fe3c3af20eb56ff425bb32
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-16 21:08:41 +0300

    add fields and constructor to Main class

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,5 +1,24 @@
 package domain;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dao.CityDAO;
+import dao.CountryDAO;
+import org.hibernate.SessionFactory;
+
 public class Main {
-    
+    private final SessionFactory sessionFactory;
+    //private final RedisClient redisClient;
+    private final ObjectMapper mapper;
+
+    private final CityDAO cityDAO;
+    private final CountryDAO countryDAO;
+
+    public Main() {
+        //sessionFactory = prepareRelationalDb();
+        cityDAO = new CityDAO(sessionFactory);
+        countryDAO = new CountryDAO(sessionFactory);
+
+        //redisClient = prepareRedisClient();
+        mapper = new ObjectMapper();
+    }
 }
