9aa08d71b4acce573a2b180bf1834d48d88174f2
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-16 20:31:20 +0300

    create entity class City with required fields

--- src/main/java/domain/City.java
+++ src/main/java/domain/City.java
@@ -1,2 +1,28 @@
-package domain;public class City {
+package domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(schema = "world", name = "city")
+@Data
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "district", nullable = false)
+    private String district;
+
+    @Column(name = "population", nullable = false)
+    private Integer population;
+
 }
